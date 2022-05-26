e67da10b017f0da81cd717b6d8dbaa9f06cfab0f
Creiton <thalmaicorreia7@gmai.com>
2022-05-26 16:49:03 -0300

    Commit inicial

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Home.css"
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">HOME</h1>
+        
+        <img src="https://i.imgur.com/H88yIo2.png" alt="Foto Tela Inicial" width={400} height= {400}/>
+
+        </>
+    );
+}
+
+export default Home;
