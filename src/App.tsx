9e3266cc5fba40e571614a1d4fdaab62d5a334f0
Creiton <thalmaicorreia7@gmai.com>
2022-05-27 13:19:00 -0300

    Novo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Home from './paginas/home/Home'
+import Home from './paginas/home/Home';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
