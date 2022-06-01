62ddf19eb07e0cd302e42e4e949f1cbb77f238a3
Creiton <thalmaicorreia7@gmai.com>
2022-06-01 09:25:00 -0300

    Adicionou Paginas Home e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './paginas/home/Home';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
+import Login  from './paginas/login/Login';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
+          <Router>
+            <Navbar />
+            <div style={{ minHeight: '100vh'}}>
+              <Routes> // Antigo Switch
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+              </Routes>
+            </div>
+            <Footer />
+          </Router>
   );
 }
 
